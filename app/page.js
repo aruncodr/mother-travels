69965ba09c374899172b75100be676fe0cbbc6cc
aruncodr <96330935+aruncodr@users.noreply.github.com>
2024-01-18 01:59:14 +0530

    Add files via upload

--- app/page.js
+++ app/page.js
@@ -24,7 +24,9 @@ export default function Home() {
   return (
     <div className=' md:px-16 text-[16px]'>
 
-      <div className='px-4 grid grid-cols-1 3xl:grid-cols-6 font-[#6B6B6B] -----Hero Page'>
+{/*---------------Hero Page  Booking Section------------*/}
+
+      <div className='px-4 grid grid-cols-1 3xl:grid-cols-6 font-[#6B6B6B]'>
 
           <div className='col-span-2  -----Left hero'>
 
@@ -61,7 +63,8 @@ export default function Home() {
           </div>
 
       </div>
-        
+{/* -------------------On Screen Button-------------------- */}
+
         <div className='w-12 h-12 fixed bottom-4 right-4'>
           <ImageLink src="/whatsapp.png" href="https://wa.me/+919092131783"/>
         </div>
@@ -73,59 +76,128 @@ export default function Home() {
           <div className='pl-2'>Call</div> 
           </div>
         </a>
-        {/* ------------Our Services----------- */}
 
+{/* -------------------Our Services-------------------- */}
 
-<div className='mt-32 text-center bg-[#FBFCFF] text-[#1C4FA2] font-semibold font- text-3xl py-8'>Our Services</div>
-        <div className='grid grid-cols-2 leading-7 mx-2 p-2 pb-8 border-1 text-[#1C4FA2] bg-[#FBFCFF] text-md font-medium'>
-          <div className='bg-white rounded-md shadow-sm text-center py-2 mx-2 my-2 hover:bg-gray-50'>Airport Services</div>
-          <div className='row-span-3 bg-white flex flex-col-reverse items-center rounded-md shadow-sm text-center py-2 mx-2 my-2 hover:bg-gray-50'>
-            <div className='font-semibold'>24 * 7 <br />Taxi services</div> <div><Image className='mb-4' width={36} height={36} src="ontime.svg"></Image></div>
-            </div>
-          <div className='bg-white rounded-md shadow-sm text-center py-2  mx-2 my-2 hover:bg-gray-50'>Outstation Rides</div>
-          <div className='grid row-span-2 bg-white rounded-md shadow-sm text-center py-2 mx-2 my-2 hover:bg-gray-50'>Local Rides</div>
-          <div className='row-span-3 bg-white flex flex-col-reverse items-center rounded-md shadow-sm text-center py-2 mx-2 my-2 hover:bg-gray-50'>
-            <div className='font-semibold'>Well Trained<br />Drivers</div> <div><Image className='mb-4' width={40} height={40} src="welldriver.svg"></Image></div>
-            </div>
-          <div className='grid row-span-2 bg-white rounded-md shadow-sm text-center py-2 mx-2 my-2 hover:bg-gray-50'>Mother travels Puducherry</div>
-          <div className='grid row-span-3 bg-white rounded-md shadow-sm text-center py-2 mx-2 my-2 hover:bg-gray-50'>Low Fare<br />Best Prices</div>
-          <div className='grid row-span-3 bg-white rounded-md shadow-sm text-center py-2 mx-2 my-2 hover:bg-gray-50'>Luxury cars<br />A/C cabs<br />Carriers</div>
-          
-        </div>
+  <div className='w-full bg-[#FBFCFF]'>
+  <div className='mt-32 text-center text---[#1C4FA2] text-[#19468F] font-semibold text-[24px] pt-8 pb-4'>Our Services</div>
 
+    <div className='grid grid-cols-2 w-full p-2 pb-12 border-1 text-[#1C4FA2] bg-[#FBFCFF] text-md font-semibold'>
 
-          <div className='ml-5 md:ml-[234px] mt-12 text-sm'>Trusted and Reviewed by </div>
-          <div className='mx-4 md:mx-72 mt-6 flex justify-between'>
+      <div className='col-span-2 mx-2 text-center bg-white rounded-xl border border-blue-50 shadow-sm py-4 mb-4 hover:bg-gray-50'>
+        <Image className='mb-4 mx-auto' width={36} height={36} src="ontime.svg"></Image>
+        <div className='font-semibold'>24 * 7 <br />Taxi services</div>
+      </div>
+
+      <div className='mx-2 text-center bg-white rounded-xl border border-blue-50 shadow-sm py-4 mr-2 hover:bg-gray-50'>
+        <Image className='mb-4 mx-auto' width={40} height={40} src="welldriver.svg"></Image>
+        <div className='font-semibold'>Well Trained<br />Drivers</div>
+      </div>
+
+      <div className='mx-2 text-center bg-white rounded-xl border border-blue-50 shadow-sm py-4 ml-2 hover:bg-gray-50'>
+        <Image className='mb-4 mx-auto' width={40} height={40} src="cost.svg"></Image>
+        <div className='font-semibold'>Low Fare<br />Best Prices</div>
+      </div>
+
+    </div>
+  </div>
+
+{/* --------------Review section 1 star ----------*/}
+<div>
+  
+</div>
+{/* --------------Review section 2 star ----------*/}
+    <div>
+          <div className='mx-6 md:ml-[234px] mt-12 text-sm'>Trusted and Reviewed by </div>
+          <div className='mx-6 mb-16 md:mx-72 mt-6 flex justify-between'>
 
           <div>
             <Image src='/google.png' width={80} height={20} alt='logo'/>
-              <div className='flex gap-1 my-2'>
-                <Image src='/star.png' width={20} height={20} alt='star'/>
-                <Image className='ml-1' src='/star.png' width={20} height={20} alt='star'/>
-                <Image className='ml-1' src='/star.png' width={20} height={20} alt='star'/>
-                <Image className='ml-1' src='/star.png' width={20} height={20} alt='star'/>
-                <Image className='ml-1' src='/starhalf.png' width={10} height={10} alt='star'/>  
+              <div className='flex my-2'>
+                <Image src='/star.png' width={14} height={14} alt='star'/>
+                <Image className='ml-1' src='/star.png' width={14} height={14} alt='star'/>
+                <Image className='ml-1' src='/star.png' width={14} height={14} alt='star'/>
+                <Image className='ml-1' src='/star.png' width={14} height={14} alt='star'/>
+                <Image className='ml-1' src='/starhalf.png' width={7} height={7} alt='star'/>  
               </div>
             <div className='text-[12px] my-2'>Rating : 4.1/5 reviews</div>
-            <button className='mt-1 py-1 md:ml-6 bg-white text-[14px] font-medium w-full text-[#1C4FA2] border-[#1C4FA2] rounded-lg'>See prices</button>
+            
           </div>
 
           <div>
             <Image className='mt-1' src='/justdial.png' width={80} height={20} alt='logo'/>
               <div className='flex mt-4'>
-                <Image src='/star.png' width={20} height={20} alt='star'/>
-                <Image className='ml-1'  src='/star.png' width={20} height={20} alt='star'/>
-                <Image className='ml-1' src='/star.png' width={20} height={20} alt='star'/>
-                <Image className='ml-1' src='/star.png' width={20} height={20} alt='star'/>
-                <Image className='ml-1' src='/starhalf.png' width={10} height={10} alt='star'/>  
+                <Image src='/star.png' width={14} height={14} alt='star'/>
+                <Image className='ml-1'  src='/star.png' width={14} height={14} alt='star'/>
+                <Image className='ml-1' src='/star.png' width={14} height={14} alt='star'/>
+                <Image className='ml-1' src='/star.png' width={14} height={14} alt='star'/>
+                <Image className='ml-1' src='/starhalf.png' width={7} height={7} alt='star'/>  
               </div>
             <div className='text-[12px] my-2'>Rating : 4.5/5 reviews</div>
           </div>
 
         </div>
-        <div>
+    </div>
 
+{/*---------------Best place in Puduchery-------------
+    <div className='mb-12 py-6 bg-[#FBFCFF]'>
+      <div className='text-xl mb-4 font-medium text-center text-[#1C4FA2]'>Best Place to visit Pondicherry</div>
+      <div className='text-left ml-12 text-[13px] leading-6 text-[#0C2347] flex'>
+        <div className='pr-6'>
+          <div>Auroville</div>
+          <div>Pondy Marina</div>
+          <div>Ocean Park</div>
+          <div>Paradise Beach</div>
+          <div>Eden Beach</div>
         </div>
+        <div>
+          <div>Manakula Vinayagar</div>
+          <div>Light House</div>
+          <div>Botanical Garden</div>
+          <div>Seaside Promenade</div>
+          <div>Bharati Government Park</div>
+        </div>
+      </div>
+    </div>
+
+
+{/*---------------Contact 1-------------*/}
+<div> 
+  <div className='mt-32 text-center text-[#19468F] font-semibold text-[24px] pt-12 pb-4'>Contact</div>
+
+  <div className='grid grid-cols-2 mt-4 mx-4 text-[#19468F]'>  
+    <div className='mx-2 text-center bg-white rounded-2xl border border-blue-50 shadow-sm py-4 mb-4 hover:bg-gray-50'>
+      <Image className='mx-auto py-2' width={26} height={26} src='admincall.svg' />
+      <div className='text-md font-semibold'>Talk to us</div>
+    </div>
+    <div className='mx-2 text-center bg-white rounded-2xl border border-blue-50 shadow-sm py-4 mb-4 hover:bg-gray-50'>
+      <Image className='mx-auto py-2' width={32} height={32} src='adminmsg.svg' />
+      <div className='text-md font-semibold'>Customer Support</div>
+    </div>
+  </div>
+</div>
+
+{/*---------------Contact 3-------------*/}
+<div className='border border-blue-50 text-[#19468F] shadow-sm rounded-2xl mx-6 px-4 pt-2 pb-4 '>
+      <div className='py-2 font-bold text-lg'> Mother travels ( Puducherry ) </div>
+      <div className='leading-7'>
+        <div className='pt-2 font-semibold'> Address</div>
+        <div className='text-md'>14, Nallavadu Road</div>
+        <div className='text-md'>Thavalakuppam</div>
+        <div className='text-md'>Puducherry</div>
+        <div className='text-md'>605 007</div>
+      </div>
+      <div className='font-semibold pt-4'>Phone</div>
+      <div>+919092131783</div>
+    </div>
+
+{/*---------------Contact 2-------------*/}
+  <div className='rounded-2xl border border-blue-50 mx-6 mb-32 mt-5'>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3904.639195049036!2d79.79768177453086!3d11.860509638264213!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a549f14fb19e809%3A0x1cf17691736c6c8e!2sMother%20Travels!5e0!3m2!1sen!2sin!4v1705503912329!5m2!1sen!2sin" className='w-full h-64 border border-gray-200 rounded-xl' allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  </div>
+
+
+
 
 
 
